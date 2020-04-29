5639ee1e7a27db8eb3422baa08a7ce7b7693a288
Benjamin <bcbenc5@hotmail.co.uk>
2020-04-29 15:01:09 +0100

    Updates

---
+++ precache-manifest.c4277f633a88d4eb6c8dcc0e41e06e2d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1eb77ae370c1c1a33d0b40001c217f2",
+    "url": "/the-mint-collection/index.html"
+  },
+  {
+    "revision": "1c12f4177cd83a719299",
+    "url": "/the-mint-collection/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "74c480dd3049674b07ff",
+    "url": "/the-mint-collection/static/js/2.bfd6b98d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/the-mint-collection/static/js/2.bfd6b98d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c12f4177cd83a719299",
+    "url": "/the-mint-collection/static/js/main.ca48cb9f.chunk.js"
+  },
+  {
+    "revision": "52b5d7034d8982241dc9",
+    "url": "/the-mint-collection/static/js/runtime-main.3258b844.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/the-mint-collection/static/media/logo.5d5d9eef.svg"
+  }
+]);
